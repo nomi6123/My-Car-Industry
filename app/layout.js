9165461f9883c5d23fc59c329539ee1ae9346220
Nomi 6123 <codewithnoman6123@gmail.com>
2025-02-19 18:58:05 +0500

    i add the script

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,12 +22,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Header/>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        
+        {/* ✅ Corrected Google AdSense Script */}
+        <Script
+          strategy="afterInteractive"
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7225725941807740"
+          crossOrigin="anonymous"
+        />
+        
+        <Header />
         {children}
-        <Footer/>
+        <Footer />
+        
       </body>
     </html>
   );
